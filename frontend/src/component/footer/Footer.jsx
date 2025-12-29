5a933bcff0897bd32764d798fd8f6edacbee0d8e
P YATHIN MACHAIAH <p.yathinmachaiah18@gmail.com>
2025-12-29 11:13:45 +0000

    frontend/src/component/footer/Footer.jsx

--- frontend/src/component/footer/Footer.jsx
+++ frontend/src/component/footer/Footer.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+
 const Footer = () => {
-  const currentUser= useSelector(state=>state.user.currentUser)
+  const currentUser = useSelector((state) => state.user.currentUser);
+
   return (
     <>
       <div className="footer-container">
         <div className="about-footer">
-           
           <p className="logo">
              NoteSharing
           </p>
@@ -21,28 +22,27 @@ const Footer = () => {
               Users can upload own notes or view/search for any notes, can create own awesome profile or can chat with any other users 
             </p>
           </div>
+        </div>
 
-          <div className="Contact-contribute-container">
-              <a href="mailto:ankitloharshi@gmail.com" className="about-us-title" target="blank">Contact Us</a>   
-              <a href="https://github.com/ankitkr437/NoteSharing" className="about-us-title" target="blank">Want to contribute</a>   
+        <div className="footer-bottom-container">
+          <p className="copyright">
+            NoteSharing Project fsd
+          </p>
+          <div className="footer-link">
+            <Link to="/" className="footer-link-item" style={{textDecoration:"none"}}>
+              Home
+            </Link>
+            <Link 
+              to={currentUser ? "/profile/" + currentUser._id : "/login"} 
+              className="footer-link-item" 
+              style={{textDecoration:"none"}}
+            >
+              Profile
+            </Link>
+            <Link to="/searchuser" className="footer-link-item" style={{textDecoration:"none"}}>
+               Chat
+            </Link>
           </div>
-          </div>
-
-          <div className="footer-bottom-container">
-            <p className="copyright">
-            Copyright © 2023 NoteSharing. All Rights Reserved
-            </p>
-            <div className="footer-link">
-              <Link to="/" className="footer-link-item" style={{textDecoration:"none"}}>
-                Home
-              </Link>
-              <Link to={"/profile/"+currentUser._id} className="footer-link-item" style={{textDecoration:"none"}}>
-                Profile
-              </Link>
-              <Link to="/searchuser" className="footer-link-item" style={{textDecoration:"none"}}>
-                 Chat
-              </Link>
-            </div>
         </div>
       </div>
     </>
